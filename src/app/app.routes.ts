b52c8dc1cb3456533dd552d1bbf9a14e0f5ba3b7
Gabriel Kiyoto <120345849+Nagi1867@users.noreply.github.com>
2025-03-07 14:40:54 -0300

    Estrutura inicial

---
+++ src/app/app.routes.ts
@@ -0,0 +1,9 @@
+import { LoginComponent } from './pagess/login/login.component';
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+];
